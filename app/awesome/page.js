51b198b82ae9604d98489a3c4cea3149863fe24d
Sameer Watts <sameerwatts00@gmail.com>
2024-01-11 18:35:41 +0530

    Editing the first app

---
+++ app/awesome/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AwesomePage = () => {
+    return (
+        <main>
+          <h1>Next js is awesome</h1>  
+        </main>
+    );
+};
+
+export default AwesomePage;
