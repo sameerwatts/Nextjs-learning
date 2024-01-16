f33ce963e2e0139bd20b479739506af413d4afd6
Sameer Watts <sameerwatts00@gmail.com>
2024-01-16 20:23:12 +0530

    Using more custom components

--- app/components/main-header/main-header.js
+++ app/components/main-header/main-header.js
@@ -2,26 +2,30 @@ import Link from "next/link";
 import Image from "next/image";
 
 import logoImg from "@/assets/logo.png";
-import classes from './main-header.module.css';
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
 
 const MainHeader = () => {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={logoImg} alt="A plate with food on it" priority/>
-        NextLevel food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
