4ab6a1f188e0c2b0bb2f5d8371f0cee26cde25f3
Sameer Watts <sameerwatts00@gmail.com>
2024-01-16 14:09:23 +0530

    Complete next js essentials exercise

--- app/meals/[mealsSlug]/page.js
+++ app/meals/[mealsSlug]/page.js
@@ -3,7 +3,7 @@ const SharePage = ({params}) => {
     return (
       <main>
         <h1>Share page inside the meal page</h1>
-        <p>{params.slug}</p>
+        <p>{params.mealsSlug}</p>
       </main>
     );
   };
