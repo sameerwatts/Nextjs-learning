aeee6a9d37d32019b2278d66550fcef9c8325d0f
Sameer Watts <sameerwatts00@gmail.com>
2024-01-17 20:01:43 +0530

    using client component efficiently

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,8 +4,10 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
+
   return (
     <>
       <MainHeaderBackground />
@@ -17,10 +19,10 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
