370e688613cdb0fa3caa6ad308e9e713f9162e26
Sameer Watts <sameerwatts00@gmail.com>
2024-01-17 21:25:00 +0530

    Fetching data by leveraging nextjs

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
-const MealsPage = () => {
+import { Meals } from "@/lib/meals";
+const MealsPage = async() => {
+  const meals = await Meals();
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +19,7 @@ const MealsPage = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals}/>
       </main>
     </>
   );
