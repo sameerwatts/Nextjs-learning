0b3d214bfbc1070abbb715f8b2c17b48ebfb50ae
Sameer Watts <sameerwatts00@gmail.com>
2024-01-18 14:34:25 +0530

    Throwing Not Found Errors For Individual Meals

--- app/meals/[mealsSlug]/page.js
+++ app/meals/[mealsSlug]/page.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import { notFound } from "next/navigation";
+
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+
 const MealDetailPage = ({ params }) => {
   const meal = getMeal(params.mealsSlug);
 
-  meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
     <>
       <header className={classes.header}>
