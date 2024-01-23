aad65a3fe9c1c1ee5b5ae718de704ed182cb023a
Sameer Watts <sameerwatts00@gmail.com>
2024-01-23 16:03:51 +0530

    Navigating Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,13 +1,22 @@
 import { useRouter } from "next/router";
 
 const ClientProjectsPage = () => {
+  const router = useRouter();
 
-    const router = useRouter();
+  function loadProjectHandler() {
+    // load data...
+    // router.push("/clients/sam/projectA");
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "sam", clientprojectid: "projectA" },
+    });
+  }
 
-    console.log("router query in clientprojectspage",router.query);
+  console.log("router query in clientprojectspage", router.query);
   return (
     <div>
       <h1>The projects of given client</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
     </div>
   );
 };
