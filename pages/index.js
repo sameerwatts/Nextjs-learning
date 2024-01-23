f2a4b9254c4669160bb1d6972462afb6a90236f8
Sameer Watts <sameerwatts00@gmail.com>
2024-01-23 16:40:15 +0530

    Planning The Project

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
-import Link from "next/link";
+import React from 'react';
 
 const HomePage = () => {
-  return (
-    <div>
-      <h1>The Home page</h1>
-      <ul>
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link href="/clients">Clients</Link>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Home page</h1>
+        </div>
+    );
 };
 
 export default HomePage;
--- pages/portfolio/list.js
+++
@@ -1,9 +0,0 @@
-const ListPage = () => {
-    return (
-        <div>
-            <h1>The List page</h1>
-        </div>
-    );
-};
-
-export default ListPage;
