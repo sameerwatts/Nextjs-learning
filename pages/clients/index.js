049e7b2ffb6542c00e648c5e7224393cf55f7977
Sameer Watts <sameerwatts00@gmail.com>
2024-01-23 15:49:53 +0530

    Navigating To Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 const ClientsPage = () => {
+  const clients = [
+    { name: "Sameer", id: "sam" },
+    { name: "Parul", id: "paru" },
+  ];
   return (
     <div>
       <h1>The clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
