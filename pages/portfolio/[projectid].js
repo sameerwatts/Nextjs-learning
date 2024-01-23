4f0b621ff4ee0a8856bbbe02917b3bc97b6d85c2
Sameer Watts <sameerwatts00@gmail.com>
2024-01-23 13:21:35 +0530

    Adding Dynamic Paths & Routes

---
+++ pages/portfolio/[projectid].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PortfolioProjectPage = () => {
+    return (
+        <div>
+            <h1>The Portfolio Project page</h1>
+        </div>
+    );
+};
+
+export default PortfolioProjectPage;
